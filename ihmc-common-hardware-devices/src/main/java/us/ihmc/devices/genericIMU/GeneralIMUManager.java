17852637ee76cae835a6d316799fe6c8cec03d31
Stefan <161661671+stefanfasano@users.noreply.github.com>
2025-07-02 15:07:32 -0500

    feature/switch-to-hashmaps (#2)
    * changed managers to take in maps when reading and writing. changed some imports
    
    * added more mechanism interface methods for stator temp

--- ihmc-common-hardware-devices/src/main/java/us/ihmc/devices/genericIMU/GeneralIMUManager.java
+++ ihmc-common-hardware-devices/src/main/java/us/ihmc/devices/genericIMU/GeneralIMUManager.java
@@ -25,6 +25,8 @@ import us.ihmc.yoVariables.registry.YoRegistry;
 import us.ihmc.yoVariables.variable.YoBoolean;
 import us.ihmc.yoVariables.variable.YoDouble;
 
+import java.util.Map;
+
 public class GeneralIMUManager implements IMUManagerInterface
 {
    /**
@@ -105,9 +107,11 @@ public class GeneralIMUManager implements IMUManagerInterface
    }
 
    @Override
-   public void read(ImuData measuredIMUData)
+   public void read(Map<String, ImuData> measuredIMUDataMap)
    {
       yoIMU.update();
+
+      ImuData measuredIMUData = measuredIMUDataMap.get(name);
 
       trueNorthInSensorFrame.setIncludingFrame(ReferenceFrame.getWorldFrame(), YoIMUMahonyFilter.NORTH_REFERENCE);
       trueNorthInSensorFrame.changeFrame(imuFrame);
