01be448303c16900921483a7cbcbca93967effe2
Biomech OCU <jli@ihmc.org>
2025-07-17 13:14:20 -0500

    Added comments to xml description

--- ihmc-common-hardware-devices/src/main/java/us/ihmc/commonHardware/xmlDescription/XmlHardwareDescription.java
+++ ihmc-common-hardware-devices/src/main/java/us/ihmc/commonHardware/xmlDescription/XmlHardwareDescription.java
@@ -25,51 +25,86 @@ public class XmlHardwareDescription
    @XmlElement
    protected XmlSettings settings;
 
+   /**
+    * @return An {@code XmlDevices} object containing all the devices in the xml
+    */
    public XmlDevices getDevices()
    {
       return devices;
    }
 
+   /**
+    * Set the devices on the robot
+    * @param devices An {@code XmlDevices} object containing the devices on the robot
+    */
    public void setDevices(XmlDevices devices)
    {
       this.devices = devices;
    }
 
+   /**
+    * @return An {@code XmlTransmissions} object containing all the transmissions in the xml
+    */
    public XmlTransmissions getTransmissions()
    {
       return transmissions;
    }
 
+   /**
+    * Set the transmissions on the robot
+    * @param transmissions An {@code XmlTransmissions} object containing the transmissions on the robot
+    */
    public void setTransmissions(XmlTransmissions transmissions)
    {
       this.transmissions = transmissions;
    }
 
+   /**
+    * @return An {@code XmlJoints} object containing all the joints in the xml
+    */
    public XmlJoints getJoints()
    {
       return joints;
    }
 
+   /**
+    * Set the joints on the robot
+    * @param joints An {@code XmlJoints} object containing the joints on the robot
+    */
    public void setJoints(XmlJoints joints)
    {
       this.joints = joints;
    }
 
+   /**
+    * @return An {@code XmlPriority} object containing the priority parameters of the robot
+    */
    public XmlPriority getPriority()
    {
       return priority;
    }
 
+   /**
+    * Set the priority parameters on the robot
+    * @param priority An {@code XmlPriority} object containing the priority parameters on the robot
+    */
    public void setPriority(XmlPriority priority)
    {
       this.priority = priority;
 	}
 
+   /**
+    * @return An {@code XmlSettings} object containing the ros settings of the robot
+    */
    public XmlSettings getSettings()
    {
       return settings;
    }
 
+   /**
+    * Set the ros settings on the robot
+    * @param settings An {@code XmlSettings} object containing the ros settings on the robot
+    */
    public void setSettings(XmlSettings settings)
    {
       this.settings = settings;
