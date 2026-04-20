d4df09f6dc262eed752d3791879a47d3acd58efd
Robert <rjgriffin42@gmail.com>
2026-04-20 16:12:43 -0500

    did a bugfix on potentially filtering imu outlier data;

--- ihmc-common-hardware-devices/src/main/java/us/ihmc/commonHardware/devices/genericSensor/YoGenericIMU.java
+++ ihmc-common-hardware-devices/src/main/java/us/ihmc/commonHardware/devices/genericSensor/YoGenericIMU.java
@@ -3,16 +3,18 @@ package us.ihmc.commonHardware.devices.genericSensor;
 import us.ihmc.commonHardware.devices.IMUInterface;
 import us.ihmc.commonHardware.devices.YoSensorInterface;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
-import us.ihmc.euclid.referenceFrame.interfaces.FrameVector3DReadOnly;
 import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
 import us.ihmc.yoVariables.euclid.YoVector3D;
-import us.ihmc.yoVariables.euclid.referenceFrame.YoFrameVector3D;
 import us.ihmc.yoVariables.registry.YoRegistry;
+import us.ihmc.yoVariables.variable.YoBoolean;
 import us.ihmc.yoVariables.variable.YoDouble;
+import us.ihmc.yoVariables.variable.YoInteger;
 
 public class YoGenericIMU implements YoSensorInterface
 {
-   protected static final ReferenceFrame worldFrame = ReferenceFrame.getWorldFrame();
+   // The observed outlier value is on the y axis only, and it's always a value at 17.45 rad/s, that we've observed.
+   private static final double GYRO_OUTLIER_THRESHOLD = 15.0;
+   private static final double MAX_TICKS_TO_IGNORE_GYRO = 20;
 
    protected final String name = getClass().getSimpleName();
    protected final YoRegistry registry;
@@ -24,6 +26,8 @@ public class YoGenericIMU implements YoSensorInterface
    protected final YoVector3D linearAccelerationBias;
    protected final YoVector3D angularVelocityBias;
    protected final YoVector3D unbiasedAngularVelocity;
+   protected final YoBoolean gyroDataIgnoredAsOutlier;
+   protected final YoInteger gyroDataIgnoredCounts;
    protected final YoDouble imuTemp;
 
    /**
@@ -45,6 +49,8 @@ public class YoGenericIMU implements YoSensorInterface
       linearAccelerationBias = new YoVector3D(prefix + "LinearAccelBias", registry);
       unbiasedAngularVelocity = new YoVector3D(prefix + "UnbiasedAngularVel", registry);
       angularVelocityBias = new YoVector3D(prefix + "AngularVelBias", registry);
+      gyroDataIgnoredAsOutlier = new YoBoolean(prefix + "GyroDataIgnoredAsOutlier", registry);
+      gyroDataIgnoredCounts = new YoInteger(prefix + "GyroDataIgnoredCounts", registry);
 
       imuTemp = new YoDouble(prefix + "IMUTemp", registry);
 
@@ -54,7 +60,19 @@ public class YoGenericIMU implements YoSensorInterface
    @Override
    public void update()
    {
-      angularVelocity.set(imu.getGyroX(), imu.getGyroY(), imu.getGyroZ());
+      boolean xIsPossiblyOutlier = Math.abs(imu.getGyroX()) > GYRO_OUTLIER_THRESHOLD;
+      boolean yIsPossiblyOutlier = Math.abs(imu.getGyroY()) > GYRO_OUTLIER_THRESHOLD;
+      boolean zIsPossiblyOutlier = Math.abs(imu.getGyroZ()) > GYRO_OUTLIER_THRESHOLD;
+      gyroDataIgnoredAsOutlier.set(checkIfOnlyOneAxisIsOutlier(xIsPossiblyOutlier, yIsPossiblyOutlier, zIsPossiblyOutlier));
+      if (gyroDataIgnoredAsOutlier.getBooleanValue())
+         gyroDataIgnoredCounts.increment();
+      else
+         gyroDataIgnoredCounts.set(0);
+
+      // Check if the data is showing up as outliers. If it doesn't show up as an outlier, then update the gyroscope data. Also, if we've been ignoring it for
+      // too long, that means it's probably not actually an outlier, so we can update the gyroscope data.
+      if (!gyroDataIgnoredAsOutlier.getBooleanValue() || gyroDataIgnoredCounts.getIntegerValue() > MAX_TICKS_TO_IGNORE_GYRO)
+         angularVelocity.set(imu.getGyroX(), imu.getGyroY(), imu.getGyroZ());
 
       linearAcceleration.set(imu.getAccelX(), imu.getAccelY(), imu.getAccelZ());
 
@@ -62,6 +80,17 @@ public class YoGenericIMU implements YoSensorInterface
       unbiasedLinearAcceleration.add(linearAcceleration, linearAccelerationBias);
 
       imuTemp.set(imu.getTemp());
+   }
+
+   private static boolean checkIfOnlyOneAxisIsOutlier(boolean xIsPossiblyOutlier, boolean yIsPossiblyOutlier, boolean zIsPossiblyOutlier)
+   {
+      if (xIsPossiblyOutlier && !yIsPossiblyOutlier && !zIsPossiblyOutlier)
+         return true;
+      if (yIsPossiblyOutlier && !xIsPossiblyOutlier && !zIsPossiblyOutlier)
+         return true;
+      if (zIsPossiblyOutlier && !xIsPossiblyOutlier && !yIsPossiblyOutlier)
+         return true;
+      return false;
    }
 
    /**
